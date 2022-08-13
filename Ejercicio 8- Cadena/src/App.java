c8a7e0054595cd38728004438b407e606440ffa2
Mauro Jelvez Bosco <mauro_jelvez@hotmail.com>
2022-08-13 16:46:35 -0300

    ejercicio 8 terminado

--- Ejercicio 8- Cadena/src/App.java
+++ Ejercicio 8- Cadena/src/App.java
@@ -10,15 +10,15 @@ b) Método invertirFrase(), deberá invertir la frase ingresada y mostrarla por 
 ejemplo: Entrada: "casa blanca", Salida: "acnalb asac".
 c) Método vecesRepetido(String letra), recibirá un carácter ingresado por el usuario y
 contabilizar cuántas veces se repite el carácter en la frase, por ejemplo: 
-d) Entrada: frase = "casa blanca". Salida: El carácter 'a' se repite 4 veces.
-e) Método compararLongitud(String frase), deberá comparar la longitud de la frase que 
+Entrada: frase = "casa blanca". Salida: El carácter 'a' se repite 4 veces.
+d) Método compararLongitud(String frase), deberá comparar la longitud de la frase que 
 compone la clase con otra nueva frase ingresada por el usuario.
-f) Método unirFrases(String frase), deberá unir la frase contenida en la clase Cadena con 
+e) Método unirFrases(String frase), deberá unir la frase contenida en la clase Cadena con 
 una nueva frase ingresada por el usuario y mostrar la frase resultante.
-g) Método reemplazar(String letra), deberá reemplazar todas las letras “a” que se 
+f) Método reemplazar(String letra), deberá reemplazar todas las letras “a” que se 
 encuentren en la frase, por algún otro carácter seleccionado por el usuario y mostrar la 
 frase resultante.
-h) Método contiene(String letra), deberá comprobar si la frase contiene una letra que 
+g) Método contiene(String letra), deberá comprobar si la frase contiene una letra que 
 ingresa el usuario y devuelve verdadero si la contiene y falso si no. */
 import Entidad.Cadena;
 import java.util.Scanner;
@@ -31,14 +31,34 @@ public class App {
     private static Scanner sn = new Scanner(System.in);
 
     public static void main(String[] args) {
-        //Se instancia un objeto Cadena
+        // Se instancia un objeto Cadena
         Cadena cadena = new Cadena();
-        //se le pide al usuario que asigne el valor del atributo frase por teclado"
+        // se le pide al usuario que asigne el valor del atributo frase por teclado"
         System.out.println("Ingrese una frase");
         cadena.setFrase(sn.nextLine());
-        //Se asigna el atributo longitud del objeto Cadena automaticamente segun la longitud del atributo frase
+        // Se asigna el atributo longitud del objeto Cadena automaticamente segun la
+        // longitud del atributo frase
         cadena.setLongitud(cadena.getFrase().length());
-        
+        // Se llama al método que cuenta las vocales del atriburo frase
+        cadena.mostrarVocales();
+        // Se llama al método que imprime por pantalla el valor invertido del atributo
+        // frase
+        cadena.invertirFrase();
+        // Se llama al método que contabiliza cuantas veces se repite un caracter dado
+        // en el atributo frase
+        cadena.vecesRepetido("a");
+        // Se llama al método que compara la longitud del atributo frase con otra frase
+        // pasada por parámetro;
+        cadena.compararLongitud("hola mundo");
+        // Se llama al método que le concatena un String pasado por parámetro al final
+        // del atributo frase
+        cadena.unirFrases("hola mundo");
+        // Se llama al método que reemplaza las letras 'a' del atributo frase por un
+        // caracter pasado por parámetro
+        cadena.reemplazar("@");
+        // se llama al método que compueba si la frase contiene un caracter pasado por
+        // parámetro
+        System.out.println(cadena.contiene("E"));
 
     }
 }
--- Ejercicio 8- Cadena/src/Entidad/Cadena.java
+++ Ejercicio 8- Cadena/src/Entidad/Cadena.java
@@ -32,19 +32,90 @@ public class Cadena {
         this.longitud = longitud;
     }
 
+    // To string
+    @Override
+    public String toString() {
+        return "Cadena [frase=" + frase + ", longitud=" + longitud + "]";
+    }
+
     // Método que contabiliza la cantidad de vocales que tiene la frase ingresada.
     public void mostrarVocales() {
         int count = 0;
         for (int i = 0; i < this.longitud; i++) {
-            if (this.frase.toUpperCase().substring(i, i + 1) == "A" || this.frase.substring(i, i + 1) == "E"
-                    || this.frase.substring(i, i + 1) == "I" || this.frase.substring(i, i + 1) == "O"
-                    || this.frase.substring(i, i + 1) == "U") {
+            if (this.frase.toUpperCase().charAt(i) == 'A' || this.frase.toUpperCase().charAt(i) == 'E'
+                    || this.frase.toUpperCase().charAt(i) == 'I' || this.frase.toUpperCase().charAt(i) == 'O'
+                    || this.frase.toUpperCase().charAt(i) == 'U') {
                 count++;
             }
         }
         System.out.println("La frase tiene " + count + " vocales");
     }
-    // Método invertirFrase(), deberá invertir la frase ingresada y mostrarla por pantalla. Por 
-    //ejemplo: Entrada: "casa blanca", Salida: "acnalb asac".
-    
+
+    // Método que invierte la frase ingresada y la muestra por pantalla
+    public void invertirFrase() {
+        for (int i = this.longitud - 1; i >= 0; i--) {
+            System.out.print(this.frase.charAt(i));
+        }
+        System.out.println("");
+    }
+
+    // Método que recibe un carácter ingresado por el usuario y contabilizar cuántas
+    // veces se repite el carácter en la frase
+    public void vecesRepetido(String letra) {
+        int count = 0;
+        for (int i = 0; i < this.longitud; i++) {
+            if (this.frase.toUpperCase().charAt(i) == letra.toUpperCase().charAt(0)) {
+                count++;
+            }
+
+        }
+        System.out.println("el caracter " + letra + " se repite " + count + " veces");
+    }
+
+    // Método que compara la longitud de la frase que compone la clase con otra
+    // nueva frase ingresada por el usuario.
+    public void compararLongitud(String frase) {
+        if (this.longitud > frase.length()) {
+            System.out.println(
+                    "la longitud de la frase ingresada es menor que la longitud del atributo frase del objeto");
+        } else if (this.longitud == frase.length()) {
+            System.out.println(
+                    "la longitud de la frase ingresada es igual que la longitud del atributo frase del objeto");
+        } else {
+            System.out.println(
+                    "la longitud de la frase ingresada es mayor que la longitud del atributo frase del objeto");
+        }
+    }
+
+    // Método que une la frase contenida en el atributo frase con una nueva frase
+    // ingresada por el usuario y muestra la frase resultante.
+    public void unirFrases(String frase) {
+        System.out.println(this.frase.concat(" "+frase));
+    }
+
+    // Método que reemplaza todas las letras “a” que se encuentren en la frase, por
+    // algún otro carácter seleccionado por el usuario y muestra la frase
+    // resultante.
+    public void reemplazar(String letra) {
+        for (int i = 0; i < this.longitud; i++) {
+            if (this.frase.toLowerCase().charAt(i) == 'a') {
+                System.out.print(letra);
+            } else {
+                System.out.print(this.frase.charAt(i));
+            }
+
+        }
+        System.out.println("");
+    }
+
+    // Método que comprueba si la frase contiene una letra que ingresa el usuario
+    public boolean contiene(String letra) {
+        boolean flag = false;
+        for (int i = 0; i < this.longitud; i++) {
+            if (this.frase.toLowerCase().charAt(i) == frase.charAt(0)) {
+                flag = true;
+            }
+        }
+        return flag;
+    }
 }
